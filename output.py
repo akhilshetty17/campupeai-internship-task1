ebe51719756f27112f0050e989e648e7a5d12a4e
Akhil M <akhilshetty017@gmail.com>
2026-02-17 10:52:40 +0530

    Added Output Py Script

---
+++ output.py
@@ -0,0 +1,8 @@
+print("Lorem Epsum Ist",end="")
+print("Um Lorem")
+
+print(1)
+print(300)
+print(5+17)
+
+print("I am Akhil,I am",21,"years old")
---
+++ script.py
@@ -0,0 +1,8 @@
+print("Hello World")
+print("This is my first program")
+a=17
+name = "Akhil Shetty"
+
+
+if 10>7:
+    print("10 is greater than 7")
